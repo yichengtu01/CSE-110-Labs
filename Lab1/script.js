c3ea183c0ec0f4fad371c473428d19433a0d701c
yichengtu <13092373+yichengtu@user.noreply.gitee.com>
2024-09-30 23:37:08 -0700

    Task 5 alert not working

--- Lab1/script.js
+++ Lab1/script.js
@@ -1,13 +1,14 @@
-const SubformID = "subscribe_form";
 
 function getSubForm(){
-    var subform = document.getElementById(SubformID);
+    var subform = document.getElementById("subscribe_form");
     console.log(subform.getAttributeNode("id").value);
     return subform;
 }
 
-function alertSubFunc(){
-    alert("Hello! I am an alert box!!");
+function alertSubFunc() {
+    Event.preventDefault();
+    let alrt = "Subscriber added: " + document.getElementById("emailtextbox").value;
+    alert(alrt);
 }
 
 window.onload = function() {
